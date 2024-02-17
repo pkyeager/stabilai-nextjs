b4f28304c75fabeac7aff43524da1686fe54e144
Phongsakon Konrad <124145979+pkyeager@users.noreply.github.com>
2024-02-17 07:28:01 +0100

    Update settings.json and hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,11 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
-/* eslint-disable react/no-unescaped-entities */
-"use client";
-import React from "react";
-
 export default function Hero() {
-  const heroImgPath = "/heroImg.jpg"; // Adjust the path as needed
-
+  const heroImgPath = "/heroImg.jpg";
   return (
     <div className="h-screen flex flex-col items-center justify-center text-center px-4 sm:px-0 relative">
       <img
@@ -19,11 +14,11 @@ export default function Hero() {
       <h1 className="text-6xl sm:text-9xl font-bold text-white z-10 hover:text-purple-500 transition-colors duration-200">
         Our Mission.
       </h1>
-      <h2 className="mt-10 text-xl sm:text-3xl font-bold text-white z-10 hover:text-purple-500 transition-colors duration-200 w-1/2">
+      <h2 className="mt-10 text-xl sm:text-3xl font-bold text-white z-10 hover:text-purple-500 transition-colors duration-200 w-full sm:w-1/2">
         Your personal strength coach in your pocket. With AI-driven workouts and
         real-time feedback, unleash your strength potential like never before.
       </h2>
-      <div className="flex justify-center items-center mt-4 space-x-5 z-10">
+      <div className="flex flex-col sm:flex-row justify-center items-center mt-4 sm:space-x-5 z-10 space-y-5">
         <a
           href="https://apps.apple.com/your-app-url"
           target="_blank"
