949510f2b5a20142e392dd756d3c77fff8be1cb7
phongsakon <pk@yeager.gmbh>
2024-04-19 13:47:18 +0200

    Update App Store link in Hero component
    The App Store link in the Hero component has been updated to point to the correct application. The Google Play Store link was removed as part of this change.

--- components/hero.tsx
+++ components/hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
       </h2>
       <div className="flex flex-col sm:flex-row justify-center items-center mt-4 sm:space-x-5 z-10 space-y-5 sm:space-y-0">
         <a
-          href="https://apps.apple.com/your-app-url"
+          href="https://apps.apple.com/app/stabil-ai/id6496847994"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:scale-105 transition-transform duration-200"
@@ -29,18 +29,6 @@ export default function Hero() {
             style={{ width: "200px", height: "" }}
             src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
             alt="Download on the App Store"
-          />
-        </a>
-        <a
-          href="https://play.google.com/store/apps/details?id=your-app-id"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:scale-105 transition-transform duration-200"
-        >
-          <img
-            style={{ width: "230px", height: "100px" }}
-            src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-            alt="Get it on Google Play"
           />
         </a>
       </div>
